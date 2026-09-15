ebcd6c23d43161499bcccdd9523c60ba34307ff6
ሸዋ ጠቅላይ ግዛት <shewateqlaygizat2019@gmail.com>
2026-09-15 06:02:04 +0300

    የተቀላቀለ ፋይል ማስወገድ

--- src/routes/finance.js
+++
@@ -1,8 +0,0 @@
-// finance.js
-module.exports = (app) => {
-  // Define routes related to finance here
-  // Example:
-  app.get('/finance', (req, res) => {
-    res.send('Finance route');
-  });
-};
